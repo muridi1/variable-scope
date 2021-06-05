711b4853a1677dfe4dcb56b3aa37aa5b7b483ef0
mohamed <muridi51@gmail.com>
2021-06-05 12:07:30 +0100

    fixing examples 0c and 0a

--- examples/0c-independent-loop-scopes.js
+++ examples/0c-independent-loop-scopes.js
@@ -5,14 +5,14 @@ console.log("Start of programme");
 const healthyFoods = ['fruit', 'veg'];
 const junkFoods = ['chocolate', 'cake', 'poison'];
 
-for (let healthyItem of healthyFoods) {
-  console.log(`${healthyItem} is normally considered a healthy food`);
+for (let Item of healthyFoods) {
+  console.log(`${Item} is normally considered a healthy food`);
 }
 
-for (let junkItem of junkFoods) {
-  console.log(`${junkItem} is NOT normally considered a healthy food`);
+for (let Item of junkFoods) {
+  console.log(`${Item} is NOT normally considered a healthy food`);
   // below will error. Why?
-  console.log(`...so, swap it for ${healthyItem}!`);
+  console.log(`...so, swap it for ${Item}!`);
 }
 
 console.log("Reached the end of my programme!");
